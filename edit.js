c4155d46d73b33870674470ad2f009bc72f4c960
Linnnus <linnnus@users.noreply.github.com>
2023-12-29 15:17:12 +0100

    Initial commit

---
+++ edit.js
@@ -0,0 +1,104 @@
+// UPLOAD INPUT ----------------------------------------------------------------
+
+const inputArea = document.getElementById("input");
+
+async function updateContent() {
+  console.debug("Sending updated input...");
+  const response = await fetch(`/update/${window.helloHtmlProjectId}`, {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: inputArea.value,
+  });
+  if (!response.ok) {
+    alert(
+      `Failed to update contents on server: ${response.statusText}. See dev console for more info.`,
+    );
+  } else {
+	  console.debug("Finished updating input");
+  }
+}
+
+function debounce(fn, delay) {
+  let timerId;
+  return function (...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
+
+inputArea.addEventListener("input", debounce(updateContent, 2000));
+
+// SMART INPUT -----------------------------------------------------------------
+
+inputArea.addEventListener("keydown", (event) => {
+  const t = event.target;
+  if (event.key === "Tab") {  // In-/dedent with tab key
+    // Don't switch to next element.
+    // This is super bad for accesability.
+    event.preventDefault();
+
+    if (t.selectionStart === t.selectionEnd) {
+      if (event.shiftKey) {
+        // We want to (optionally) remove a single tab character at the start of the current line.
+        // TODO
+      } else {
+        // We use execCommand so the edit history isn't flushed.
+        // See: https://meta.discourse.org/t/add-better-undo-support-when-inserting-formatted-text/214821/13
+        document.execCommand("insertText", false, "\t");
+      }
+    } else {
+      // Record initial selection. What the user sees.
+      const oldSelectionStart = t.selectionStart;
+      const oldSelectionEnd = t.selectionEnd;
+
+      // Move visual selection back to start of line.
+      while (t.selectionStart !== 0 && t.value[t.selectionStart-1] !== "\n") {
+        t.selectionStart -= 1;
+      }
+
+      if (event.shiftKey) { // remove tab from each line
+        const text = t.value.slice(t.selectionStart, t.selectionEnd);
+        const newText = text.split("\n").map(l => l.startsWith("\t") ? l.slice(1) : l).join("\n");
+        console.assert(newText.length <= text.length);
+
+        document.execCommand("insertText", false, newText);
+
+        // Restore visual selection.
+        // We use oldSelectionStart to avoid cursor jumping to beginning of line.
+        // We use oldSelectionEnd because execCommand fucked t.selectionEnd.
+        const firstLine = text.split("\n", 1)[0];
+        t.selectionStart = firstLine.startsWith("\t") ? oldSelectionStart - 1 : oldSelectionStart;
+        t.selectionEnd = oldSelectionEnd + (newText.length - text.length);
+      } else {
+        const text = t.value.slice(t.selectionStart, t.selectionEnd);
+        const newText = text.split("\n").map(l => "\t" + l).join("\n");
+        console.assert(newText.length > text.length);
+
+        document.execCommand("insertText", false, newText);
+
+        const firstLine = text.split("\n", 1)[0];
+        t.selectionStart = /\S/.test(firstLine) ? oldSelectionStart + 1 : oldSelectionStart;
+        t.selectionEnd = oldSelectionEnd + (newText.length - text.length);
+      }
+    }
+  } else if (event.key == "Enter") { // Copy indentation on newline
+    if (t.selectionStart === t.selectionEnd) { // no visual selection
+      event.preventDefault(); // don't insert linebreak - we will do that
+      const lines = t.value.split("\n");
+      const currentLine = lines[t.value.slice(0, t.selectionEnd).split("\n").length - 1];
+      const indent = currentLine.match(/^\s*/)[0];
+      document.execCommand("insertText", false, "\n" + indent);
+    }
+  }
+});
+
+// OUTPUT ----------------------------------------------------------------------
+
+const outputFrame = document.getElementById("output");
+const eventSource = new EventSource(`/listen/${window.helloHtmlProjectId}`);
+eventSource.addEventListener("message", (_) => {
+  console.debug("hellohtml: Content has changed on server. Reloading iframe.");
+  outputFrame.contentWindow.location.reload();
+});
---
+++ server.ts
@@ -0,0 +1,135 @@
+import { HandlerContext, router } from "https://deno.land/x/rutt@0.1.0/mod.ts";
+import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
+
+interface Project {
+  content: string;
+}
+
+const kv = await Deno.openKv();
+
+function serveFile(path: string, init?: ResponseInit): () => Response {
+  const contents = Deno.readFileSync(path);
+  return () => new Response(contents, init);
+}
+
+// This endpoint creates a new project and redirects the client to the edit page.
+const DEFAULT_CONTENT = Deno.readTextFileSync("default.html");
+async function handleNew(): Promise<Response> {
+  const newId = crypto.randomUUID();
+  const project = { content: DEFAULT_CONTENT };
+  await kv.set(["projects", newId], project);
+  return new Response(null, {
+    status: 302,
+    headers: {
+      "Location": `/edit/${newId}`,
+    },
+  });
+}
+
+// This endpoint presents the client with a ui to interact with the given project.
+// The client-side JavaScript is populated with some data in a rather hacky way...
+const EDIT_HTML = Deno.readTextFileSync("edit.html");
+async function handleEdit(
+  _req: Request,
+  _ctx: HandlerContext,
+  { id }: Record<"id", string>,
+): Promise<Response> {
+  const { value: project } = await kv.get(["projects", id]);
+  if (project === null) {
+    return new Response(`project with id ${id} not found`, { status: 404 });
+  }
+
+  const pageContent = EDIT_HTML
+    .replaceAll("{{ID}}", id)
+    .replaceAll("{{content}}", project.content);
+  return new Response(pageContent, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+}
+
+// This endpoint returns an IFrame-embeddable HTML page for the given project.
+async function handleView(
+  _req: Request,
+  _ctx: HandlerContext,
+  { id }: Record<"id", string>,
+): Promise<Response> {
+  const { value: project } = await kv.get<Project>(["projects", id]);
+  if (project === null) {
+    return new Response(`Content for project with id ${id} not found`, {
+      status: 404,
+    });
+  }
+
+  return new Response(project.content, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+}
+
+// This endpoint is used by the client to update the project's source code.
+async function handleUpdate(
+  req: Request,
+  _ctx: HandlerContext,
+  { id }: Record<"id", string>,
+): Promise<Response> {
+  const { value: project } = await kv.get<Project>(["projects", id]);
+  if (project === null) {
+    return new Response(`Content for project with id ${id} not found`, {
+      status: 404,
+    });
+  }
+
+  const newContent = await req.text();
+  project.content = newContent;
+  await kv.set(["projects", id], project);
+
+  return new Response(null, { status: 204, statusText: "Updated database" });
+}
+
+// The client needs to be notified of changes so it can reload the view.
+// This endpoint responds with a classic event stream, as values in the DB are updated.
+function handleListen(
+  _req: Request,
+  _ctx: HandlerContext,
+  { id }: Record<"id", string>,
+): Response {
+  const toValueEventStream = new TransformStream({
+    start(controller) {
+      controller.enqueue(`retry: 1000\n\n`);
+    },
+    transform(chunk, controller) {
+      for (const { value } of chunk) {
+        controller.enqueue(`data: ${JSON.stringify(value)}\n\n`);
+      }
+    },
+  });
+  const stream = kv.watch([["projects", id]])
+    .pipeThrough(toValueEventStream)
+    .pipeThrough(new TextEncoderStream());
+  return new Response(stream, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+    },
+  });
+}
+
+const handler = router({
+  // FIXME: NGINX or something should serve these files ngl.
+  "/": serveFile("index.html"),
+  "edit.js": serveFile("edit.js"),
+  "/new": handleNew,
+  "/edit/:id": handleEdit,
+  "/view/:id": handleView,
+  "/update/:id": handleUpdate,
+  "/listen/:id": handleListen,
+});
+
+const port = 8080;
+console.log(`HTTP server running. Access it at: http://0.0.0.0:${port}/`);
+await serve(handler, { port });
+
+// vi: ft=typescript et ts=2 sw=2
